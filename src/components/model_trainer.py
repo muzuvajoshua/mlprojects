5e6e1d6ba2171b9366142ad482fd67a19fca3909
muzuvajoshua <muzuvajoshua@gmail.com>
2024-12-10 17:52:39 +0300

    performed HyperParameter tuning with GridSearchCV

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -44,8 +44,42 @@ class ModelTrainer:
                 "XGBoost": XGBRegressor(),
                 "CatBoost": CatBoostRegressor(verbose=False)
                 }
+            
+            params = {
+                    "Random Forest": {
+                        "n_estimators": [8,16,32,64,128,256],
+                    },
+                    "Gradient Boosting": {
+                        "n_estimators": [8,16,32,64,128,256],
+                        "learning_rate": [.1, .01, .05, .001],
+                        "sub_sample": [0.6,0.7,0.75,0.8,0.85,0.9],
+                    },
+                    "AdaBoost": {
+                        "n_estimators": [8, 16, 32, 64, 128, 256],
+                        "learning_rate": [0.1,0.01 ,0.5, .001]
+                    },
+                    "Linear Regression": {},
+                    "KNN": {
+                        "n_neighbors": [5, 7, 9, 11],
+                    },
+                    "Decision Tree": {
+                        "criterion": ["squared_error", "friedman_mse", "absolute_error", "poisson"]
+                    },
+                    "XGBoost": {
+                        "n_estimators": [8,16,32,64,128,256],
+                        "learning_rate": [.1, .01, .05, .001],
+                           },
+                    "CatBoost": {
+                        "iterations": [30, 50, 100],
+                        "depth": [6, 8, 10],
+                        "learning_rate": [0.01, 0.05, 0.1],
+                    }
+                }
+
+            
+
             model_report: dict = evaluate_model(X_train=X_train,y_train=y_train,
-                                                X_test=X_test,y_test=y_test,models= models)
+                                                X_test=X_test,y_test=y_test,models= models,param = params)
             
             #getting the best model score
             best_model_score = max(sorted(model_report.values()))
